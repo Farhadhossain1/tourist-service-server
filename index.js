eef76702950d627c3182fce7b502de34d7724798
Forhad <farhadhossain01992@gmail.com>
2022-11-11 19:16:41 +0600

    Review Dleted

--- index.js
+++ index.js
@@ -59,6 +59,39 @@ async function run(){
             const result = await cursor.toArray();
             res.send(result);
         })
+
+        app.get("/myReviews", async(req, res)=>{
+            let query = {};
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result); 
+        })
+
+        app.delete("/myReviews/:id", async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result  = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
     finally{
 
